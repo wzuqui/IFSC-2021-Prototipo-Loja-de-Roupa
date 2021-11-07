1fc92fb008a417a4588b113d32ba27ec18b62d1d
ImaKrp <julio.c04@aluno.ifsc.edu.br>
2021-11-07 16:25:08 -0300

    pop

--- src/js/pop.js
+++ src/js/pop.js
@@ -1,1 +1,14 @@
-nao
+const modalwrapper = document.querySelector(".modalwrapper");
+const pop = document.querySelector(".pop");
+
+function openPop() {
+  pop.classList.add("active");
+  modalwrapper.style.display = "block";
+  modalwrapper.addEventListener("click", closePop);
+}
+
+function closePop() {
+  pop.classList.remove("active");
+  modalwrapper.style.display = "none";
+  modalwrapper.removeEventListener("click", closePop);
+}
