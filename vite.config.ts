ed680fb77150845d98413e14fec089eb95f4b525
Willian Zuqui <willian.zuqui@ravex.com.br>
2023-08-29 18:57:20 -0300

    ci: public

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://wzuqui.github.io/IFSC-2021-Prototipo-Loja-de-Roupa/',
   plugins: [react()],
-})
+});
